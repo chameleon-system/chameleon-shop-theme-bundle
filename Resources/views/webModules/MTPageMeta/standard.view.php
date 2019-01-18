ab83fa22cfd6f1bc170aeee85a182b9be9fb3d94
Ulrich Kuhn <ulrich.kuhn@esono.de>
2019-01-18 13:13:35 +0100

    ref #275: Add WIP for hreflang header output

--- Resources/views/webModules/MTPageMeta/standard.view.php
+++ Resources/views/webModules/MTPageMeta/standard.view.php
@@ -1,25 +1,33 @@
 <title><?=TGlobal::OutHTML($data['sTitle']); ?></title>
-<?php if ($sCanonical) {
-    ?><link rel="canonical" href="<?=$sCanonical; ?>" /><?php
-} ?>
 <?php
-  foreach ($data['aMetaData'] as $metaType => $metaData) {
-      if (!is_array($metaData)) {
-          if (!empty($metaData)) {
-              echo '<meta '.TGlobal::OutHTML($metaType).'="'.TGlobal::OutHTML($metaData)."\" />\n";
-          }
-      } else {
-          foreach ($metaData as $key => $content) {
-              if ('http-equiv' != $metaType) {
-                  $content = TGlobal::OutHTML($content);
-              }
-              if (!empty($content)) {
-                  echo '<meta '.TGlobal::OutHTML($metaType).'="'.TGlobal::OutHTML($key).'" content="'.$content."\" />\n";
-              }
-          }
-      }
-  }
-?>
-<?php if (!empty($data['sCustomHeaderData'])) {
+if ($sCanonical) {
+    ?><link rel="canonical" href="<?=$sCanonical; ?>" /><?php
+}
+foreach ($data['aMetaData'] as $metaType => $metaData) {
+    if (!is_array($metaData)) {
+        if (!empty($metaData)) {
+            echo '<meta '.TGlobal::OutHTML($metaType).'="'.TGlobal::OutHTML($metaData)."\" />\n";
+        }
+    } else {
+        foreach ($metaData as $key => $content) {
+            if ('http-equiv' != $metaType) {
+                $content = TGlobal::OutHTML($content);
+            }
+            if (!empty($content)) {
+                echo '<meta '.TGlobal::OutHTML($metaType).'="'.TGlobal::OutHTML($key).'" content="'.$content."\" />\n";
+            }
+        }
+    }
+}
+
+// TODO what about other MTPageMeta/standard.view.php ("non-theme")?
+
+if (\count($data['language-alternatives']) > 0) {
+    foreach ($data['language-alternatives'] as $iso => $url) {
+        echo sprintf('<link rel="alternate" href="%s" hreflang="%s"/>', $url, $iso);
+    }
+}
+if (!empty($data['sCustomHeaderData'])) {
     echo $data['sCustomHeaderData'];
-} ?>
+}
+?>
