2644de8e550d5fff1ff3d61f01b570e8f3b0cca8
Stefan Kruppa <stefan@umulmrum.de>
2019-04-10 09:54:03 +0200

    Fix CS

--- Resources/views/layoutTemplates/export.layout.php
+++ Resources/views/layoutTemplates/export.layout.php
@@ -4,14 +4,14 @@ use ChameleonSystem\CoreBundle\ServiceLocator;
 
 $isCmsTemplateEngineEditMode = ServiceLocator::get('chameleon_system_core.request_info_service')->isCmsTemplateEngineEditMode();
     if (true === $isCmsTemplateEngineEditMode) {
-    ?>
+        ?>
     <!doctype html>
     <head>
         <!--#CMSHEADERCODE#-->
     </head>
     <body>
 <?php
-} ?>
+    } ?>
 <?php $modules->GetModule('primary'); ?>
 <?php if (true === $isCmsTemplateEngineEditMode) {
         ?>
