f8364289ec6d3f1fafe0bbceb9721446f23949b0
Sven Jansen <sven.jansen@esono.de>
2024-12-20 15:45:49 +0100

    ref #65248: refactor usage of TShopArticle::GetDetailLink auf getLink

--- Resources/views/objectviews/pkgShop/views/db/TShopArticle/atom-froogle.view.php
+++ Resources/views/objectviews/pkgShop/views/db/TShopArticle/atom-froogle.view.php
@@ -1,47 +1,47 @@
 <?php
-  /**
-   * shows an article preview inc category.
-   *
-   * @var $oArticle      TdbShopArticle
-   * @var $sMessages     string
-   * @var $aCallTimeVars array
-   */
-  $oLocal = TCMSLocal::GetActive();
-  $sDetailLink = $oArticle->GetDetailLink(true);
+/**
+ * shows an article preview inc category.
+ *
+ * @var $oArticle      TdbShopArticle
+ * @var $sMessages     string
+ * @var $aCallTimeVars array
+ */
+$oLocal = TCMSLocal::GetActive();
+$sDetailLink = $oArticle->getLink(true);
 
-  $sInfoText = $oArticle->fieldUsp;
-  if (empty($sInfoText)) {
-      $sInfoText = $oArticle->fieldSubtitle;
-  }
+$sInfoText = $oArticle->fieldUsp;
+if (empty($sInfoText)) {
+    $sInfoText = $oArticle->fieldSubtitle;
+}
 
-  $oManufacturer = $oArticle->GetFieldShopManufacturer();
+$oManufacturer = $oArticle->GetFieldShopManufacturer();
 
-  $oImages = $oArticle->GetFieldShopArticleImageList();
+$oImages = $oArticle->GetFieldShopArticleImageList();
 
 ?>
 <entry>
-  <title><?=$oArticle->GetName(); ?></title>
-  <g:id><?=$oArticle->id; ?></g:id>
-  <link href="<?=$sDetailLink; ?>" />
-  <g:price><?=$oArticle->dPrice; ?></g:price>
-  <descriptiontype="html"><![CDATA[ <?=$sInfoText; ?> <?=$oArticle->fieldDescription; ?> ]]></description>
+  <title><?php echo $oArticle->GetName(); ?></title>
+  <g:id><?php echo $oArticle->id; ?></g:id>
+  <link href="<?php echo $sDetailLink; ?>" />
+  <g:price><?php echo $oArticle->dPrice; ?></g:price>
+  <descriptiontype="html"><![CDATA[ <?php echo $sInfoText; ?> <?php echo $oArticle->fieldDescription; ?> ]]></description>
 
-  <g:ean><?=$oArticle->fieldArticlenumber; ?></g:ean>
-  <g:isbn><?=$oArticle->fieldArticlenumber; ?></g:isbn>
+  <g:ean><?php echo $oArticle->fieldArticlenumber; ?></g:ean>
+  <g:isbn><?php echo $oArticle->fieldArticlenumber; ?></g:isbn>
   <?php if ($oArticle->GetReviewAverageScore() > 0) {
-    ?><g:rating><?=round($oArticle->GetReviewAverageScore()); ?> stars</g:rating><?php
-} ?>
+      ?><g:rating><?php echo round($oArticle->GetReviewAverageScore()); ?> stars</g:rating><?php
+  } ?>
   <?php if (!is_null($oManufacturer)) {
-        ?><g:publisher><?=$oManufacturer->fieldName; ?></g:publisher><?php
-    }?>
-  <g:publish_date><?=$oArticle->fieldDatecreated; ?></g:publish_date>
+      ?><g:publisher><?php echo $oManufacturer->fieldName; ?></g:publisher><?php
+  }?>
+  <g:publish_date><?php echo $oArticle->fieldDatecreated; ?></g:publish_date>
 
   <?php
-    $iMaxImageCount = 10;
-    while ($iMaxImageCount > 0 && ($oImg = $oImages->Next())) {
-        --$iMaxImageCount;
-        $oCMSImage = $oImg->GetImage(0, 'cms_media_id');
-        echo '<g:image_link>'.$oCMSImage->GetFullURL()."</g:image_link>\n";
-    }
-  ?>
+  $iMaxImageCount = 10;
+while ($iMaxImageCount > 0 && ($oImg = $oImages->Next())) {
+    --$iMaxImageCount;
+    $oCMSImage = $oImg->GetImage(0, 'cms_media_id');
+    echo '<g:image_link>'.$oCMSImage->GetFullURL()."</g:image_link>\n";
+}
+?>
 </entry>
--- Resources/views/objectviews/pkgShop/views/db/TShopArticlePreviewImage/standard.view.php
+++ Resources/views/objectviews/pkgShop/views/db/TShopArticlePreviewImage/standard.view.php
@@ -1,34 +1,34 @@
 <?php
-   /**
-    * used to display a preview image for an article.
-    */
-   /* @var $oArticle TdbShopArticle // the calling article */
-   /* @var $oArticleSize TdbShopArticleImageSize // the image size to use */
-   /* @var $oImage TCMSImage // the tcms image to show */
-   /* @var $oImageThumbnail TCMSImage // the tcms image to show cut to the right size based on the oArticleSize object*/
-   /* @var $oPreviewImage TdbShopArticlePreviewImage - the preview image object */
+/**
+ * used to display a preview image for an article.
+ */
+/* @var $oArticle TdbShopArticle // the calling article */
+/* @var $oArticleSize TdbShopArticleImageSize // the image size to use */
+/* @var $oImage TCMSImage // the tcms image to show */
+/* @var $oImageThumbnail TCMSImage // the tcms image to show cut to the right size based on the oArticleSize object */
+/* @var $oPreviewImage TdbShopArticlePreviewImage - the preview image object */
 
-   $sDetailLink = '';
-   $sDetailLinkSeoName = '';
-   if ($oArticle->IsVariant()) {
-       $oParent = $oArticle->GetFieldVariantParent();
-       $sDetailLink = $oParent->GetDetailLink();
-       $sDetailLinkSeoName = $oParent->GetNameSEO();
-   } else {
-       $sDetailLink = $oArticle->GetDetailLink();
-       $sDetailLinkSeoName = $oArticle->GetNameSEO();
-   }
+$sDetailLink = '';
+$sDetailLinkSeoName = '';
+if ($oArticle->IsVariant()) {
+    $oParent = $oArticle->GetFieldVariantParent();
+    $sDetailLink = $oParent->getLink();
+    $sDetailLinkSeoName = $oParent->GetNameSEO();
+} else {
+    $sDetailLink = $oArticle->getLink();
+    $sDetailLinkSeoName = $oArticle->GetNameSEO();
+}
 ?>
-<a href="<?=$sDetailLink; ?>">
+<a href="<?php echo $sDetailLink; ?>">
 <?php
   if ('1' == $oArticle->fieldIsNew) {
       echo '<img class="marker" src="/assets/images/icons/new.png" alt="'.TGlobal::OutHTML(TGlobal::Translate('chameleon_system_chameleon_shop_theme.marker.new')).'" />';
   }
-  if ($oArticle->fieldPriceReference > $oArticle->dPrice) {
-      $sStyle = '';
-      if ('1' == $oArticle->fieldIsNew) {
-          $sStyle = 'style="top:14px"';
-      }
-      echo '<img '.$sStyle.' class="marker" src="/assets/images/icons/sale.png" alt="Sale!" />';
-  }
-?><img class="cover" src="<?=$oImageThumbnail->GetFullURL(); ?>" alt="<?=TGlobal::OutHTML($oArticle->GetNameSEO()); ?>" border="0" style="margin-top:<?php echo(130 - $oImageThumbnail->aData['height']) / 2; ?>px" height="<?=$oImageThumbnail->aData['height']; ?>" /></a>
+if ($oArticle->fieldPriceReference > $oArticle->dPrice) {
+    $sStyle = '';
+    if ('1' == $oArticle->fieldIsNew) {
+        $sStyle = 'style="top:14px"';
+    }
+    echo '<img '.$sStyle.' class="marker" src="/assets/images/icons/sale.png" alt="Sale!" />';
+}
+?><img class="cover" src="<?php echo $oImageThumbnail->GetFullURL(); ?>" alt="<?php echo TGlobal::OutHTML($oArticle->GetNameSEO()); ?>" border="0" style="margin-top:<?php echo (130 - $oImageThumbnail->aData['height']) / 2; ?>px" height="<?php echo $oImageThumbnail->aData['height']; ?>" /></a>
